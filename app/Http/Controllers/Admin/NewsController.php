bdf9104ed92c23030e9fc26e26d26bfe7fc9afcf
azusa <azusound@gmail.com>
2020-09-22 04:31:24 +0000

    0922

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 {
     //
@@ -14,23 +16,41 @@ class NewsController extends Controller
     }
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+          $path = $request->file('image')->store('public/image');
+          $news->image_path = basename($path);
+        } else {
+          $news->image_path = null;
+        }
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+        
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
-        
     }
-}
-class ProfileController extends Controller
-{
-    //
-    public function add()
+    public function index(Request $request)
     {
-        return view('admin.profile.create');
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = News::where('title', $cond_title)->get();
+        } else {
+            $posts = News::all();
+        }
+        return view('admin.news.index', ['posts' =>$posts, 'cond_title' => $cond_title]);
     }
-    public function create(Request $request)
-    {
-        // admin/profile/createにリダイレクトする
-        return redirect('admin/profile/create');
-        
-    }  
-    
+
+
 }
