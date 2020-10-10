4e07a5eb68c513859258b84215ea19b0847fe545
azusa <azusound@gmail.com>
2020-10-10 04:11:21 +0000

    PHP/Laravel 16課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,12 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 use App\News;
-
 class NewsController extends Controller
 {
-    //
+ //
     public function add()
     {
         return view('admin.news.create');
@@ -51,6 +49,38 @@ class NewsController extends Controller
         }
         return view('admin.news.index', ['posts' =>$posts, 'cond_title' => $cond_title]);
     }
-
-
+    public function edit(Request $request)
+    {
+        $news = News::find($request->id);
+        if (empty($news)) {
+            abort(404);
+        }
+        return view('admin.news.edit', ['news_form' =>$news]);
+    }
+    
+    public function update(Request $request)
+    {
+        $this->validate($request, News::$rules);
+        $news = News::find($request->id);
+        $news_form = $request->all();
+        if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+          $news_form['image_path'] = $news->image_path;
+        }
+        unset($news_form['_token']);
+        
+        $news->fill($news_form)->save();
+        
+        return redirect('admin/news');
+    }
+    public function delete(Request $request)
+    {
+        $news = News::find($request->id);
+        $news->delete();
+        return redirect('admin/news');
+    }
 }
